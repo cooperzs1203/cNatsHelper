9b65eff10099cb580a015c2eef2e4016dfd9c1f9
cooperzs1203 <cooperzs1203@gmail.com>
2019-09-10 06:51:01 -0700

    big update

--- cNatsHelper.go
+++ cNatsHelper.go
@@ -1,28 +1,134 @@
 package cNatsHelper
 
 import (
+	"encoding/json"
 	"errors"
+	"github.com/nats-io/go-nats"
 	"os"
+	"time"
 )
 
 var (
-	serveName = ""
-	servePID = -1
+	cnh *cNatsHelper
 )
 
-func InitServe(Name string) error {
-	if Name == "" {
-		return errors.New("ServeName can not be empty")
+type SubHandler func(msg CMsg)
+
+func InitAndLoadHelper(modName , addr string) error {
+	if modName == "" || addr == "" { return errors.New("mod Name or address can't be empty") }
+
+	if cnh == nil {
+		conn , err := nats.Connect(addr)
+		if err != nil { return err }
+
+		cnh = &cNatsHelper{
+			mod: modName,
+			pid: int64(os.Getegid()),
+			conn:  conn,
+		}
 	}
-
-	serveName = Name
-	servePID = os.Getegid()
-
-	// init nats.EncodedConn
-	err := initNatsEC()
-	if err != nil {
-		return err
-	}
-
 	return nil
+}
+
+type cNatsHelper struct {
+	mod 	string
+	pid 	int64
+	conn  	*nats.Conn
+}
+
+func (c *cNatsHelper) NewMsg(mod , serve string , data []byte) CMsg {
+	cMsg := CMsg{
+		FInfo:     SerInfo{
+			Mod: c.mod,
+			Ser: "",
+			PID: c.pid,
+		},
+		TInfo:     SerInfo{
+			Mod: mod,
+			Ser: serve,
+			PID: 0,
+		},
+		TimeStamp: time.Now().Unix(),
+		Data:      data,
+	}
+	return cMsg
+}
+
+// publish task
+func (c *cNatsHelper) publish(mod , serve string , data []byte) error {
+	cMsg := c.NewMsg(mod , serve , data)
+	return c.conn.Publish(cMsg.GetToAddr() , cMsg.ToJSONBytes())
+}
+
+// do task
+func (c *cNatsHelper) subscribe(serve string , handler nats.MsgHandler) error {
+	subject := c.mod + "." + serve
+	_ , err := c.conn.Subscribe(subject , handler)
+	return err
+}
+
+// queue do task
+func (c *cNatsHelper) queueSubscribe(serve , queue string , handler nats.MsgHandler) error {
+	subject := c.mod + "." + serve
+	_ , err := c.conn.QueueSubscribe(subject , queue , handler)
+	return err
+}
+
+// sync request - response
+func (c *cNatsHelper) requestSync(mod, serve string, data []byte, timeout time.Duration) (CMsg, error) {
+	reqMsg := c.NewMsg(mod , serve , data)
+	msg , err := c.conn.Request(reqMsg.GetToAddr() , reqMsg.ToJSONBytes() , timeout)
+	if err != nil {
+		return CMsg{} , err
+	}
+
+	var rspMsg CMsg
+	err = json.Unmarshal(msg.Data , &rspMsg)
+	return rspMsg , err
+}
+
+
+
+func Publish(mod , serve string , data []byte) error {
+	if mod == "" || serve == "" {
+		return errors.New("mod , serve or handler can not be nil")
+	}
+
+	return cnh.publish(mod , serve , data)
+}
+
+func Subscribe(serve string , handler SubHandler) error {
+	if serve == "" || handler == nil {
+		return errors.New("mod , serve or handler can not be nil")
+	}
+
+	h := func(msg *nats.Msg) {
+		cMsg := CMsg{}
+		_ = json.Unmarshal(msg.Data , &cMsg)
+		handler(cMsg)
+	}
+
+	return cnh.subscribe(serve , h)
+}
+
+func QueueSubscribe(serve , queue string , handler SubHandler) error {
+	if serve == "" || handler == nil {
+		return errors.New("mod , serve , queue or handler can not be nil")
+	}
+
+	h := func(msg *nats.Msg) {
+		cMsg := CMsg{}
+		_ = json.Unmarshal(msg.Data , &cMsg)
+		handler(cMsg)
+	}
+
+	return cnh.queueSubscribe(serve , queue , h)
+}
+
+func Request(mod , serve string , data []byte , timeout time.Duration) (CMsg , error) {
+	if mod == "" || serve == "" || data == nil {
+		return CMsg{} , errors.New("mod , serve , queue or handler can not be nil")
+	}
+
+	return cnh.requestSync(mod , serve , data , timeout)
 }
--- cNatsMsg.go
+++ cNatsMsg.go
@@ -1,184 +1,28 @@
 package cNatsHelper
 
-import (
-	"fmt"
-	"github.com/nats-io/go-nats"
-	"strings"
-)
+import "encoding/json"
 
-const (
-	_REQUEST_REPLY_ = "REQUEST_REPLY"
-)
-
-type ServeInfo struct {
-	Name string
+type SerInfo struct {
+	Mod string
+	Ser string
 	PID int64
 }
 
-// CNatsMsg : For basic communication between services
-// Msg : Communication underlying information
-// serveInfo : public serve information
-type CNatsMsg struct {
-	ServeInfo ServeInfo
-	Msg nats.Msg
+type CMsg struct {
+	FInfo 		SerInfo
+	TInfo 		SerInfo
+	TimeStamp 	int64
+	Data		[]byte
 }
 
-func (cMsg *CNatsMsg) GetServeName() string {
-	serveName := ""
-	if cMsg == nil {
-		return serveName
+func (cMsg *CMsg) ToJSONBytes() []byte {
+	jsonBytes , err := json.Marshal(cMsg)
+	if err != nil {
+		return []byte("")
 	}
-
-	serveName = cMsg.ServeInfo.Name
-	return serveName
+	return jsonBytes
 }
 
-func (cMsg *CNatsMsg) GetServePID() string {
-	servePID := ""
-	if cMsg == nil {
-		return servePID
-	}
-
-	servePID = fmt.Sprintf("%d" , cMsg.ServeInfo.PID)
-	return servePID
+func (cMsg *CMsg) GetToAddr() string {
+	return cMsg.TInfo.Mod + "." + cMsg.TInfo.Ser
 }
-
-func (cMsg *CNatsMsg) GetSubject() string {
-	subject := ""
-	if cMsg == nil {
-		return subject
-	}
-
-	subject = cMsg.Msg.Subject
-	return subject
-}
-
-func (cMsg *CNatsMsg) GetMsgData() string {
-	data := ""
-	if cMsg == nil {
-		return data
-	}
-
-	data = string(cMsg.Msg.Data)
-	return data
-}
-
-func (cMsg *CNatsMsg) GetMsgDataBytes() []byte {
-	var data []byte
-	if cMsg == nil {
-		return data
-	}
-
-	data = cMsg.Msg.Data
-	return data
-}
-
-func (cMsg *CNatsMsg) GetReply() string {
-	reply := ""
-	if cMsg == nil {
-		return reply
-	}
-
-	reply = cMsg.Msg.Reply
-	return reply
-}
-
-func (cMsg *CNatsMsg) NeedReply() bool {
-	if cMsg == nil {
-		return false
-	}
-
-	return !(cMsg.Msg.Reply == "")
-}
-
-func (cMsg *CNatsMsg) GetReplyPrefix() string {
-	replyPrefix := ""
-	if cMsg == nil {
-		return replyPrefix
-	}
-
-	replyPrefix = strings.Join([]string{cMsg.GetServeName() , cMsg.GetServePID() , _REQUEST_REPLY_} , ".")
-
-	return replyPrefix
-}
-
-
-
-
-// make normal cNatsMsg for publish
-func NormalCMsg(subject string, data []byte) CNatsMsg {
-	if subject = buildSubject(subject); subject == "" {
-		return CNatsMsg{}
-	}
-
-	cMsg := CNatsMsg{
-		ServeInfo: ServeInfo{
-			Name: serveName,
-			PID:  int64(servePID),
-		},
-		Msg:       nats.Msg{
-			Subject: subject,
-			Reply: "",
-			Data: data,
-			Sub: nil,
-		},
-	}
-
-	return cMsg
-}
-
-// a cNatsMsg for request
-func RequestCMsg(subject string, data []byte) CNatsMsg {
-	if subject = buildSubject(subject); subject == "" {
-		return CNatsMsg{}
-	}
-
-	reply := buildReply()
-
-	cMsg := CNatsMsg{
-		ServeInfo: ServeInfo{
-			Name: serveName,
-			PID:  int64(servePID),
-		},
-		Msg:       nats.Msg{
-			Subject: subject,
-			Reply: reply,
-			Data: data,
-			Sub: nil,
-		},
-	}
-
-	return cMsg
-}
-
-func ReplyCMsg(reqCMsg CNatsMsg , replyData []byte) CNatsMsg {
-	cMsg := CNatsMsg{
-		ServeInfo: ServeInfo{
-			Name: serveName,
-			PID:  int64(servePID),
-		},
-		Msg:       nats.Msg{
-			Subject: "",
-			Reply: "",
-			Data: replyData,
-			Sub: nil,
-		},
-	}
-
-	if !reqCMsg.NeedReply() {
-		return cMsg
-	}
-
-	reply := reqCMsg.GetReply()
-
-	if reqCMsg.ServeInfo.Name != "" && reqCMsg.ServeInfo.PID != -1 {
-		if strings.HasPrefix(reply, reqCMsg.GetReplyPrefix()) {
-			reply = strings.Replace(reply , reqCMsg.GetReplyPrefix() , "" , 1)
-		}
-	}
-
-	cMsg.Msg.Subject = reply
-
-	return cMsg
-}
-
